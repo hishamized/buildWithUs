e177599941ec53650149af3adccb5f61cb78615b
hishamized <mirhashim2001@gmail.com>
2023-08-29 23:30:35 +0530

    User Interface Built for home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,20 @@
     <script src="https://kit.fontawesome.com/1bc2765d38.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     @yield('css')
+    <link rel="stylesheet" href="{{ asset('storage/css/global.css') }}">
+
 </head>
 
 <body>
     <header>
         @include('_navbar')
-
+        <br>
+        <br>
     </header>
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
 
     <!-- Additional JavaScript if needed -->
